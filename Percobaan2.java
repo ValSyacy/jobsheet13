6ff6e60a5bcd976785b01ecc3ebf47faba5ab635
ValSyacy <novalsyacy11@gmail.com>
2023-11-30 08:42:03 +0700

    Modifikasi Percobaan 2

--- Percobaan2.java
+++ Percobaan2.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
 public class Percobaan2 {
-    static int hitungPangkat (int x, int y){
-        if (y == 0){
-            return (1);
-        }else{
-            return (x * hitungPangkat(x, y - 1));
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            return 1;
+        } else {
+            return x * hitungPangkat(x, y - 1);
         }
     }
+
     public static void main(String[] args) {
-        Scanner input02 = new Scanner (System.in);
+        Scanner input02 = new Scanner(System.in);
 
         int bilangan, pangkat;
 
@@ -18,6 +19,15 @@ public class Percobaan2 {
         System.out.print("Pangkat: ");
         pangkat = input02.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        int hasil = hitungPangkat(bilangan, pangkat);
+
+        System.out.print(bilangan + "^" + pangkat + " dicetak: ");
+        for (int i = 0; i < pangkat; i++) {
+            System.out.print(bilangan);
+            if (i < pangkat - 1) {
+                System.out.print("x");
+            }
+        }
+        System.out.println(" = " + hasil);
     }
 }
